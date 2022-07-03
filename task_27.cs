2e5f1f5425b7902d5307cfd513baf2352ef5a0a4
Alena-Belousova <107402810+Alena-Belousova@users.noreply.github.com>
2022-07-03 20:18:31 +0300

    добавлена задача 27

---
+++ task_27.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе.
+Console.WriteLine("задача 27");
+Console.Write("введите число: ");
+int numb = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+if(numb/10==0)
+{
+    Console.WriteLine("число "+numb+ " однозначное" );
+    Console.WriteLine ("суммировать нечего");
+}
+else
+{
+    while(numb%100>0)
+    {
+    int b = numb%10;
+    numb=numb/10;
+    result=result+b;
+    }
+}
+Console.WriteLine("сумма цифр числа = " + result);
+
