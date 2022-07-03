23f92f6abae175d015a9ba2ace875f9997815d92
Alena-Belousova <107402810+Alena-Belousova@users.noreply.github.com>
2022-07-03 20:20:28 +0300

    добавлена задача 29

---
+++ task_29.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+Console.WriteLine("задача 29");
+int[] array=new int [8];
+int n=8;
+Console.Write("массив из восьми чисел: ");
+for(int index= 0; index<n; index++)
+{
+  array[index]  = new Random().Next(1,10);
+ 
+  Console.Write(array[index] + " ");
+  }
